5ddd381775671c43f94fc32af38a4f2403fc4d39
Ruchi Vivek Desai <ruchivdesai@gmail.com>
2015-09-03 14:42:15 -0400

    Use environment variables in place of plain text for sauce credentials

--- tests/test_redcap_hooks.py
+++ tests/test_redcap_hooks.py
@@ -41,8 +41,11 @@ class TestRedcapHooks(unittest.TestCase):
                 'browserName': "firefox",
                 'version': "40"
             }
+            self.username = os.environ['SAUCE_USERNAME']
+            self.key = os.environ['SAUCE_ACCESS_KEY']
+            self.sauce_url = "%s:%s@ondemand.saucelabs.com:80/wd/hub" % (self.username, self.key)
             self.driver = webdriver.Remote(
-                command_executor='http://ctsit:c600f49a-9697-4358-8c3e-2e74c26f9f2f@ondemand.saucelabs.com:80/wd/hub',
+                command_executor="http://%s/wd/hub" % self.sauce_url,
                 desired_capabilities=desired_cap)
             url = "http://localhost:8080"
 
