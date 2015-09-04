e258110fcb4fec1f5cc70e6d3735251f753eac24
Ruchi Vivek Desai <ruchivdesai@gmail.com>
2015-09-04 10:50:26 -0400

    Use plain text access keys
     - I observed that Sauce Labs tests only pass if I have a sauce
       connect tunnel open from my local machine. This commit is for
       verifying if we need to have a SauceConnect tunnel open from
       our local machine even when we are using unencrypted credentials

--- tests/test_redcap_hooks.py
+++ tests/test_redcap_hooks.py
@@ -45,8 +45,9 @@ class TestRedcapHooks(unittest.TestCase):
                 'browserName': "firefox",
                 'version': "40"
             }
-            sauce_url = "http://{0}:{1}@ondemand.saucelabs.com:80/wd/hub".format(
-                os.environ['SAUCE_USERNAME'],os.environ['SAUCE_ACCESS_KEY'])
+            # sauce_url = "http://{0}:{1}@ondemand.saucelabs.com:80/wd/hub".format(
+            #     os.environ['SAUCE_USERNAME'],os.environ['SAUCE_ACCESS_KEY'])
+            sauce_url = "http://ctsit:c600f49a-9697-4358-8c3e-2e74c26f9f2f@ondemand.saucelabs.com:80/wd/hub"
 
             self.driver = webdriver.Remote(
                 command_executor=sauce_url,
